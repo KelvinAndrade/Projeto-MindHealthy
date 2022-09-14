40732005bdb1dd5d04616c3b1552a362682a81d7
Kelvin <kelvinbr2748@hotmail.com>
2022-09-14 09:34:33 -0300

    Atualizando projeto

--- src/postagem/modules/postagem.module.ts
+++ src/postagem/modules/postagem.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { PostagemController } from "src/controller/postagem.controller";
+import { PostagemService } from "src/service/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Postagem])],//Esta criando a tabela, passando a entidade da Classe Tarefa
-    providers: [],
-    controllers: [],
+    providers: [PostagemService],
+    controllers: [PostagemController],
     exports: [TypeOrmModule]
 
 })
